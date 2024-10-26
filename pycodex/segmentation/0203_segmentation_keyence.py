9b325f614dc746b3fad308116853767fb360f4f1
wuwenrui555 <wuwenruiwwr@outlook.com>
2024-10-26 02:46:08 -0400

    update: segmentation for Keyence

--- pycodex/segmentation/0203_segmentation_keyence.py
+++ pycodex/segmentation/0203_segmentation_keyence.py
@@ -1,10 +1,10 @@
-import logging
 import os
 
-import tensorflow as tf
 from pycodex import io, utils
 
-# parameter ^ #####
+
+########################################################################################################################
+
 marker_dir = "/mnt/nfs/storage/RCC/RCC_formal_CODEX/RCC_TMA543/images/final"
 output_dir = "/mnt/nfs/home/wenruiwu/projects/shuli_rcc/output/data/segmentation_20241022_run1"
 
@@ -15,21 +15,9 @@ scale = True
 maxima_threshold = 0.075
 interior_threshold = 0.20
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"  # GPU setup: Specify visible GPU(s) and allow memory growth
-# parameter $ #####
+########################################################################################################################
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-try:
-    gpus = tf.config.list_physical_devices("GPU")
-    if gpus:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logging.info(f"Using GPU(s): {[gpu.name for gpu in gpus]}")
-    else:
-        logging.info("No GPU detected, using CPU.")
-except RuntimeError as e:
-    logging.info(f"GPU setup failed: {e}")
+io.setup_gpu("0,1,2,3")
 
 segmentation_dir = os.path.join(output_dir, "segmentation")
 cropped_dir = os.path.join(output_dir, "cropped")
