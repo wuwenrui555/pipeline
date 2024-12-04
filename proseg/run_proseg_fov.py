a0c7299cb5512cc99fdab290ce5556a773a18936
wuwenrui555 <wuwenruiwwr@outlook.com>
2024-12-04 13:59:21 -0500

    update: proseg

--- proseg/run_proseg_fov.py
+++ proseg/run_proseg_fov.py
@@ -7,75 +7,31 @@ import pandas as pd
 from pycodex import io
 from tqdm import tqdm
 
-from src.proseg import run_proseg_fov
+import src.proseg as proseg
 
 io.setup_logging(
     file_handler_level=logging.INFO, stream_handler_level=logging.INFO
 )
 
 
-def proseg_tma_z_as_voxel_layer(path_tx, dir_output):
-    """
-    Run proseg on TMA for each FOV with the z-layer as the voxel layer.
-    """
-    df_tx = pd.read_csv(path_tx)
-    for fov in tqdm(df_tx["fov"].unique()):
-        # Create a directory for the FOV
-        dir_fov = os.path.join(dir_output, f"{fov:03d}")
-        os.makedirs(dir_fov, exist_ok=True)
-
-        # Save the transcript file for the FOV
-        path_tx_fov = os.path.join(dir_fov, "tx_file.csv")
-        df_tx_fov = df_tx[df_tx["fov"] == fov].reset_index(drop=True)
-        df_tx_fov.to_csv(path_tx_fov, index=False)
-
-        # Get the z-layer
-        z = sorted(df_tx_fov["z"].unique())
-        z_layer = len(z)
-
-        # Run proseg
-        run_proseg_fov(
-            dir_output_fov=dir_fov,
-            path_tx_fov=path_tx_fov,
-            n_voxel_layer=z_layer,
-        )
-        logging.info(f"Proseg executed successfully for FOV {fov:03d}.")
-
-
-def proseg_fov_with_different_voxel_layers(
-    path_tx_fov, dir_output, voxel_layers
-):
-    """
-    Run proseg on FOV with different voxel layers.
-    """
-    for voxel_layer in voxel_layers:
-        # Create a directory for results
-        dir_fov = os.path.join(dir_output, f"voxel_layers={voxel_layer:02d}")
-        os.makedirs(dir_fov, exist_ok=True)
-
-        # Run proseg
-        run_proseg_fov(
-            dir_output_fov=dir_fov,
-            path_tx_fov=path_tx_fov,
-            n_voxel_layer=voxel_layer,
-        )
-        logging.info(
-            f"Proseg executed successfully with voxel_layers={voxel_layer:02d}."
-        )
-
-
 # %%
-
 if __name__ == "__main__":
-    # path_tx = "/mnt/nfs/storage/CosMX/Indepth_TMA971_section01_v132/AtoMx/flatFiles/Indepth_EBV971_CosMx/Indepth_EBV971_CosMx_tx_file.csv.gz"
-    # dir_output = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_fov_cosmx_only_Indepth_TMA971_section01_v132"
-    # proseg_z_as_voxel_layer(path_tx, dir_output)
-
-    path_tx_fov = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_fov_cosmx_only_Indepth_TMA971_section01_v132/001/tx_file.csv"
-    dir_output = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_test_voxel_layer/"
-    voxel_layers = [1, 5, 9, 20]
-    proseg_fov_with_different_voxel_layers(
-        path_tx_fov,
-        dir_output,
-        voxel_layers,
-    )
+    if False:
+        path_tx = "/mnt/nfs/storage/CosMX/Indepth_TMA971_section01_v132/AtoMx/flatFiles/Indepth_EBV971_CosMx/Indepth_EBV971_CosMx_tx_file.csv.gz"
+        dir_output = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_fov_cosmx_only_Indepth_TMA971_section01_v132"
+        proseg.proseg_tma_z_as_voxel_layer(path_tx, dir_output)
+    
+    if False:
+        path_tx_fov = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_fov_cosmx_only_Indepth_TMA971_section01_v132/001/tx_file.csv"
+        dir_output = "/mnt/nfs/home/wenruiwu/projects/indepth_cosmx_dlbcl/data/output/proseg_test_voxel_layer/"
+        voxel_layers = [1, 5, 9, 20]
+        proseg.proseg_with_different_voxel_layers(
+            path_tx_fov,
+            dir_output,
+            voxel_layers,
+        )
+    
+    if True:
+        path_tx = "/mnt/nfs/storage/CosMX/AIH_TMA001_section05_v132/AtoMx/flatFiles/AIH_TMA001_section05_v132/AIH_TMA001_section05_v132_tx_file.csv.gz"
+        dir_output = "/mnt/nfs/home/wenruiwu/projects/guanrui_aih/data/output/proseg_fov_AIH_TMA001_section05_v132"
+        proseg.proseg_tma_z_as_voxel_layer(path_tx, dir_output, fovs=[3,4,5,9,10,11,13,16,19])
--- proseg/src/proseg.py
+++ proseg/src/proseg.py
@@ -3,10 +3,13 @@ import logging
 import os
 import subprocess
 
+import pandas as pd
+from tqdm import tqdm
 
-def run_proseg_fov(
-    dir_output_fov: str,
-    path_tx_fov: str,
+
+def proseg(
+    path_tx: str,
+    dir_output: str,
     n_voxel_layer: int,
     n_threads: int = 20,
     x_column: str = "x_global_px",
@@ -17,10 +20,10 @@ def run_proseg_fov(
 
     Parameters:
     ----------
-    dir_output_fov : str
+    path_tx : str
+        Path to the transcript file.
+    dir_output : str
         Directory where the output files will be saved.
-    path_tx_fov : str
-        Path to the transcript file for the field of view.
     n_voxel_layer : int
         Voxel layers.
     n_threads : int, optional
@@ -41,16 +44,16 @@ def run_proseg_fov(
         f"--nthreads={n_threads}",
         f"--x-column={x_column}",
         f"--y-column={y_column}",
-        f"--output-expected-counts={os.path.join(dir_output_fov, 'cell-expected-counts.csv.gz')}",
-        f"--output-cell-metadata={os.path.join(dir_output_fov, 'cell-metadata.csv.gz')}",
-        f"--output-transcript-metadata={os.path.join(dir_output_fov, 'transcript-metadata.csv.gz')}",
-        f"--output-gene-metadata={os.path.join(dir_output_fov, 'gene-metadata.csv.gz')}",
-        f"--output-cell-polygons={os.path.join(dir_output_fov, 'cell-polygons.geojson.gz')}",
-        f"--output-cell-polygon-layers={os.path.join(dir_output_fov, 'cell-polygons-layers.geojson.gz')}",
-        f"--output-cell-hulls={os.path.join(dir_output_fov, 'cell-hulls.geojson.gz')}",
-        f"--output-cell-voxels={os.path.join(dir_output_fov, 'cell-voxels.csv.gz')}",
+        f"--output-expected-counts={os.path.join(dir_output, 'cell-expected-counts.csv.gz')}",
+        f"--output-cell-metadata={os.path.join(dir_output, 'cell-metadata.csv.gz')}",
+        f"--output-transcript-metadata={os.path.join(dir_output, 'transcript-metadata.csv.gz')}",
+        f"--output-gene-metadata={os.path.join(dir_output, 'gene-metadata.csv.gz')}",
+        f"--output-cell-polygons={os.path.join(dir_output, 'cell-polygons.geojson.gz')}",
+        f"--output-cell-polygon-layers={os.path.join(dir_output, 'cell-polygons-layers.geojson.gz')}",
+        f"--output-cell-hulls={os.path.join(dir_output, 'cell-hulls.geojson.gz')}",
+        f"--output-cell-voxels={os.path.join(dir_output, 'cell-voxels.csv.gz')}",
         f"--voxel-layers={n_voxel_layer}",
-        path_tx_fov,
+        path_tx,
     ]
 
     try:
@@ -60,3 +63,90 @@ def run_proseg_fov(
         logging.error(f"An error occurred during proseg execution: {e}")
     except FileNotFoundError:
         logging.error("Proseg command not found.")
+
+
+def proseg_tma_z_as_voxel_layer(path_tx, dir_output, fovs=None):
+    """
+    Run proseg on TMA for each FOV with the z-layer as the voxel layer.
+
+    Parameters:
+    ----------
+    path_tx : str
+        Path to the transcript CSV file containing data for all FOVs.
+    dir_output : str
+        Directory where the output for each FOV will be saved.
+    fovs : list, optional
+        List of FOVs to process. If None, all FOVs in the transcript file will be processed.
+
+    Returns:
+    -------
+    None
+    """
+    # Load the transcript file
+    df_tx = pd.read_csv(path_tx)
+    logging.info(f"Loaded transcript file with {len(df_tx)} records.")
+
+    # Filter by specified FOVs if provided
+    if fovs is not None:
+        df_tx = df_tx[df_tx["fov"].isin(fovs)].reset_index(drop=True)
+        logging.info(f"Filtered transcript data for specified FOVs: {fovs}.")
+
+    # Run proseg for each FOV
+    for fov in tqdm(df_tx["fov"].unique(), desc="Processing FOVs"):
+        # Create a directory for the current FOV
+        dir_fov = os.path.join(dir_output, f"{fov:03d}")
+        os.makedirs(dir_fov, exist_ok=True)
+
+        # Save the transcript file for the current FOV
+        path_tx_fov = os.path.join(dir_fov, "tx_file.csv")
+        df_tx_fov = df_tx[df_tx["fov"] == fov].reset_index(drop=True)
+        df_tx_fov.to_csv(path_tx_fov, index=False)
+        logging.info(
+            f"Saved transcript file for FOV {fov:03d} at {path_tx_fov}."
+        )
+
+        # Get the z-layer
+        z = sorted(df_tx_fov["z"].unique())
+        z_layer = len(z)
+        logging.info(f"FOV {fov:03d}: Detected {z_layer} unique z-layers.")
+
+        # Run proseg
+        try:
+            proseg(path_tx_fov, dir_fov, z_layer)
+            logging.info(f"Proseg executed successfully for FOV {fov:03d}.")
+        except Exception as e:
+            logging.error(f"Error executing proseg for FOV {fov:03d}: {e}")
+
+
+def proseg_with_different_voxel_layers(path_tx, dir_output, voxel_layers):
+    """
+    Run proseg with different voxel layers.
+
+    Parameters:
+    ----------
+    path_tx : str
+        Path to the transcript file.
+    dir_output : str
+        Directory where the output for each voxel layer configuration will be saved.
+    voxel_layers : list of int
+        List of voxel layer configurations to process.
+
+    Returns:
+    -------
+    None
+    """
+    for voxel_layer in voxel_layers:
+        # Create a directory for the current voxel layer
+        dir_fov = os.path.join(dir_output, f"voxel_layers={voxel_layer:02d}")
+        os.makedirs(dir_fov, exist_ok=True)
+
+        # Run proseg with the specified voxel layer
+        try:
+            proseg(path_tx, dir_fov, voxel_layer)
+            logging.info(
+                f"Proseg executed successfully with voxel_layers={voxel_layer:02d}."
+            )
+        except Exception as e:
+            logging.error(
+                f"Error executing proseg for voxel_layers={voxel_layer:02d}: {e}"
+            )
