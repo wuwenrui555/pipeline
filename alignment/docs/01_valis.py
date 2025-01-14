048da718c818f97bb468334d4bdd7ae942ab1b35
wuwenrui555 <wuwenruiwwr@outlook.com>
2025-01-14 13:12:10 -0500

    update alignment: create formatted alignment parameter

--- alignment/docs/01_valis.py
+++ alignment/docs/01_valis.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 from IPython.core.interactiveshell import InteractiveShell
 from tqdm import tqdm
 
@@ -24,7 +25,7 @@ def get_id(row):
 
 
 # Load alignment parameters
-params_f = "/mnt/nfs/home/wenruiwu/projects/bidmc-jiang-rcc/data/raw/20250113_alignment_parameter.xlsx"
+params_f = "/mnt/nfs/home/wenruiwu/projects/bidmc-jiang-rcc/data/raw/20250114_alignment_parameter.xlsx"
 sheets = pd.ExcelFile(params_f).sheet_names
 sheet_l = [
     sheet
@@ -42,28 +43,64 @@ cols_rename = {
     "marker_src": "src_register",
     "tma": "tma",
 }
-params_df = (
-    pd.concat(
-        [
-            pd.read_excel(params_f, sheet_name=sheet)
-            .assign(sheet=sheet)
-            .assign(tma=re.search(r"TMA\d+", sheet).group(0))
-            for sheet in sheet_l
-        ]
-    )
-    .reset_index(drop=True)
-    .rename(columns=cols_rename)
-)
+params_df = pd.concat(
+    [
+        pd.read_excel(params_f, sheet_name=sheet)
+        .assign(sheet=sheet)
+        .assign(tma=re.search(r"TMA\d+", sheet).group(0))
+        for sheet in sheet_l
+    ]
+).rename(columns=cols_rename)
 params_df["id"] = params_df.apply(get_id, axis=1)
 
-# %%
 output_root = "/mnt/nfs/home/wenruiwu/projects/bidmc-jiang-rcc/output/data/20250112_alignment_valis/"
+params_df["output_dir"] = [
+    str(Path(output_root) / "valis" / id) for id in params_df["id"]
+]
+
+dapi = np.full(params_df.shape[0], fill_value="").astype(object)
+idx_na_dst = ~params_df["dapi_dst"].isna()
+idx_na_src = ~params_df["dapi_src"].isna()
+dapi[idx_na_dst] = params_df["dapi_dst"][idx_na_dst].str.replace(
+    "^run1-", "dst_", regex=True
+)
+dapi[idx_na_src] = params_df["dapi_src"][idx_na_src].str.replace(
+    "^run2-", "src_", regex=True
+)
+params_df["dapi"] = dapi
+
+params_df = (
+    params_df[
+        [
+            "id",
+            "tma",
+            "dst_id",
+            "src_id",
+            "dst_dir",
+            "src_dir",
+            "output_dir",
+            "dst_register",
+            "src_register",
+            "dapi",
+        ]
+    ]
+    .sort_values("id")
+    .reset_index(drop=True)
+)
+
+params_df.to_csv(
+    "/mnt/nfs/home/wenruiwu/projects/bidmc-jiang-rcc/output/data/20250112_alignment_valis/alignment_parameter.csv",
+    index=False,
+)
+
+# %%
+
+# %%
+params_df
 
 
 def run_valis_alignment(output_root, row):
-    tma_id = re.sub(r"-formal", "", row["sheet"])
-    id = f"RCC-{tma_id}-dst={row['dst_id']}-src={row['src_id']}"
-    id = re.sub(r"\s+", "", id)
+    id = row["id"]
 
     dst_dir = Path(row["dst_dir"])
     src_dir = Path(row["src_dir"])
@@ -108,9 +145,25 @@ if False:
         except Exception as e:
             logging.error(f"Failed: {id} ({e}).")
 
-if True:
+if False:
     setup_logging(Path(output_root) / "valis_alignment_tma003.log")
     run_df = params_df[params_df["tma"] == "TMA003"]
+    for i, row in tqdm(
+        run_df.iterrows(),
+        desc="Processing",
+        bar_format=TQDM_FORMAT,
+        total=run_df.shape[0],
+    ):
+        id = row["id"]
+        try:
+            run_valis_alignment(output_root, row)
+            logging.info(f"Succeed: {id}.")
+        except Exception as e:
+            logging.error(f"Failed: {id} ({e}).")
+
+if False:
+    setup_logging(Path(output_root) / "valis_alignment_tma001.log")
+    run_df = params_df[params_df["id"] == "RCC-TMA001(reg3x4)-dst=reg001-src=reg001"]
     for i, row in tqdm(
         run_df.iterrows(),
         desc="Processing",
